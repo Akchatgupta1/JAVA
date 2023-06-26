9c97e496a466b1124461155efb6ed0862eda4738
Akchat Gupta <akchatg0608@gmail.com>
2023-06-26 16:11:34 +0530

    till loops

--- first_program.java
+++ first_program.java
@@ -2,8 +2,49 @@ public class first_program
 {
     public static void main(String args[])
     {
-        System.out.println("hello world");
+        /*System.out.println("hello world");
         System.out.println("hiiiii");
         System.out.println("hello world");
+        int a=12, b=12, c;
+        c=a+b;
+        System.out.println("ans is "+c);
+        
+        if(c<20){
+            System.out.println("lesser than 20");
+        }
+        else {
+            System.out.println("greater than 20");
+        }
+        int i=0,j=0,d=0;
+        for(i=0;i<=10;i++){
+            for(j=0;j<=i;j++){
+            System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for(d=0;d<=10;d++){
+         int e =5*d;
+          System.out.println(e);
+        }*/
+
+        /*int space=5;
+        for(int i=1;i<=5;i++){
+            for(int k=1;k<=space;k++ ){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            space--;
+        }*/
+           
+        for(int i=1;i<=10;i++){
+            for(int j=10;j>=1;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
